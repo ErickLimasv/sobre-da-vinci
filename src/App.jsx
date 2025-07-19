aabdeb0e266fd5e19eb3ae7127e5e3e7712c252f
Erick Lima <ericklima3784@gmail.com>
2025-07-19 15:53:05 -0300

    Definido o Setup Inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import Navbar from "./components/Navbar/Navbar";
+import Banner from "./components/Banner/Banner";
+import Section from "./components/Section/Section";
+
+import "./App.css"
+
 export default function App() {
   return (
-    <>
-    </>
+    <div className="App">
+      <header>
+        <Navbar/>
+
+        <Banner/>
+      </header>
+
+      <main>
+        <Section
+          id="curiosidades"
+        />
+        <Section
+          id="pinturas"
+        />
+        <Section
+          id="invencoes"
+        />
+      </main>
+    </div>
   );
 }
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,9 @@
+import "./Section.css"
+
+export default function Section({id}) {
+    return (
+        <section id={id}>
+            
+        </section>
+    )
+}
